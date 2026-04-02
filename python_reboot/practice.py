d49790c94531bb4d85480c36bcf1fc536810c246
Amlan <amlan2002@gmail.com>
2026-04-03 00:16:35 +0530

    add max_element

--- python_reboot/practice.py
+++ python_reboot/practice.py
@@ -8,6 +8,35 @@
 # #     result=ch+result
 # # print(result)
 
+# def word_count(s):
+#     dict={}
+#     for word in s.split():
+#         if word in dict:
+#             dict[word]+=1
+#         else:
+#             dict[word]=1
+#     print(dict)
+
+
+# s="hi him"
+# word_count(s)
+
+
+# def max_element(ls):
+#     max=0
+#     for element in ls:
+#         if element > max:
+#             max=element
+#     return max
+
+
+
+
+# list=[3, 7, 2, 10, 1]
+# max=max_element(list)
+# print(max)
+
+
 def word_count(s):
     dict={}
     for word in s.split():
@@ -18,5 +47,9 @@ def word_count(s):
     print(dict)
 
 
-s="hi him"
+
+
+
+s="hi hello him"
 word_count(s)
+
