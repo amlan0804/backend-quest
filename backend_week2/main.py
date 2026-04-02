9871b245819946a443dc8c00e76d2b19dbe2822b
Amlan <amlan2002@gmail.com>
2026-04-03 00:31:03 +0530

    add simple GET request

--- backend_week2/main.py
+++ backend_week2/main.py
@@ -2,5 +2,11 @@ from fastapi import FastAPI
 from routes import router
 
 app = FastAPI()
+@app.get('/ping')
+def ping():
+    return {"status":"ok"}
 
-app.include_router(router)
+@app.get('/')
+def root():
+    return {"message":"hello"}
+#app.include_router(router)
